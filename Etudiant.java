10d667d4c7a8b97c8c3bfb5b13618284ff34d1c0
Francoise7 <46641811+Francoise7@users.noreply.github.com>
2019-08-07 22:05:59 -0400

    Update Etudiant.java

--- Etudiant.java
+++ Etudiant.java
@@ -3,8 +3,10 @@ public class Etudiant {
 
 public static void main(String[] args){
 
+  // Rebecca pourquoi as tu modifier cette classe
 System.out.println("Hello ceci est mon deuxieme commit");
 
+  System.out.println("Coucou j'ai modifie ton fichier lolol");
 
 }
 
